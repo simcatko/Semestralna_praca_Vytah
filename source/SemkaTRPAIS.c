cd817500fb00b0ef560ee76ce20077d797ce3445
simcatko <simona.prokopova2@gmail.com>
2021-04-05 19:00:05 +0200

    Privolanie vytahu pomocou buttonu

--- source/SemkaTRPAIS.c
+++ source/SemkaTRPAIS.c
@@ -27,22 +27,45 @@
 #define BUTTON_3 0xc3
 #define BUTTON_4 0xc4
 
+#define DESTINATION_FLOOR_NONE 10
+#define DESTINATION_FLOOR_P 0
+#define DESTINATION_FLOOR_1 1
+#define DESTINATION_FLOOR_2 2
+#define DESTINATION_FLOOR_3 3
+#define DESTINATION_FLOOR_4 4
+
 #define HORNY_NARAZNIK 0xe5
 #define DOLNY_NARAZNIK 0xdf
 
 #define START_BYTE 0xA0
 #define ACK_BYTE 0xA1
 #define MY_ADDRESS 0x00
+
 #define LOCK_THE_DOOR 0x01
 #define UNLOCK_THE_DOOR 0x00
-#define DOOR_ADDRESS 0xf0
+#define ELEVATOR_ADDRESS 0xf0
+#define TERMINAL_ADDRESS 0xd0
+
+#define EMERGENCE_BREAK 0xf
+#define DEACTIVATE_BREAK 0x00
+#define ACTIVATE_BREAK 0x01
+
 #define ENGINE_ADDRESS 0xf1
 #define ENGINE_UP 70
 #define ENGINE_DOWN -70
 #define ENGINE_STOP 0
-#define MESSAGE_RECEIVER_ADDRESS 0
-#define MESSAGE_SENDER_ADDRESS 1
-#define MESSAGE_VELKOST_DAT 2
+
+#define MESSAGE_TYPE 0
+#define MESSAGE_RECEIVER_ADDRESS 1
+#define MESSAGE_SENDER_ADDRESS 2
+#define MESSAGE_VELKOST_DAT 3
+
+#define FLOOR_P_SWITCH 0xe0
+#define FLOOR_1_SWITCH 0xe1
+#define FLOOR_2_SWITCH 0xe2
+#define FLOOR_3_SWITCH 0xe3
+#define FLOOR_4_SWITCH 0xe4
+
 
 volatile uint8_t Sprava[10], Index = 0;
 volatile uint8_t Sprava_Complete = 0;
@@ -110,9 +133,10 @@ void engine(int32_t speed) {
 }
 
 void engine_speed() {
-	uint8_t vypocetcrc[] = {ENGINE_ADDRESS, MY_ADDRESS, 0x04}
-	uint8_t vysledokcrc = genrcrc(vypocetcrc, sizeof(vypocetcrc));
-	uint8_t packet[] = { START_BYTE, ENGINE_ADDRESS, MY_ADDRESS, 0x01, 0x03, vysledokcrc };
+	uint8_t vypocetcrc[] = { ENGINE_ADDRESS, MY_ADDRESS, 0x04 };
+	uint8_t vysledokcrc = gencrc(vypocetcrc, sizeof(vypocetcrc));
+	uint8_t packet[] = { START_BYTE, ENGINE_ADDRESS, MY_ADDRESS, 0x01, 0x03,
+			vysledokcrc };
 	LPSCI_WriteBlocking(UART0, packet, sizeof(packet));
 }
 void ack(uint8_t Adresa_prijimatela) {
@@ -123,15 +147,41 @@ void ack(uint8_t Adresa_prijimatela) {
 	LPSCI_WriteBlocking(UART0, packet, sizeof(packet));
 }
 
+void door(uint8_t unlock_open_door) {
+	uint8_t vypocetcrc[] = { ELEVATOR_ADDRESS, MY_ADDRESS, unlock_open_door };
+	uint8_t vysledokcrc = gencrc(vypocetcrc, sizeof(vypocetcrc));
+	uint8_t packet[] = { START_BYTE, ELEVATOR_ADDRESS, MY_ADDRESS, 0x01,
+			unlock_open_door, vysledokcrc };
+	LPSCI_WriteBlocking(UART0, packet, sizeof(packet));
+}
+
+void terminal(char *message, size_t length) {
+	uint8_t vypocetcrc[length + 2];
+	vypocetcrc[0] = TERMINAL_ADDRESS;
+	vypocetcrc[1] = MY_ADDRESS;
+	memcpy(&vypocetcrc[2], (void *)message, length);
+	uint8_t vysledokcrc = gencrc(vypocetcrc, sizeof(vypocetcrc));
+	uint8_t packet[length + 5];
+	packet[0] = START_BYTE;
+	packet[1] = TERMINAL_ADDRESS;
+	packet[2] = MY_ADDRESS;
+	packet[3] = length;
+	memcpy(&packet[4], (void *)message, length);
+	packet[length + 4] = vysledokcrc;
+	LPSCI_WriteBlocking(UART0, packet, sizeof(packet));
+}
+
 void UART0_IRQHandler(void) {
 
 	if (kLPSCI_RxDataRegFullFlag) {
 		znak = LPSCI_ReadByte(UART0);
 		switch (stav_citania) {
 		case 0:
-			if (znak == 0xA0) stav_citania = 1;
-			else if (znak == 0xA1) stav_citania = 6;
-			else process_message = 3;
+			if (znak == 0xA0 || znak == 0xA1) {
+				stav_citania = 1;
+				message[MESSAGE_TYPE] = znak;
+			} else
+				process_message = 3;
 			break;
 		case 1:
 			message[MESSAGE_RECEIVER_ADDRESS] = znak;
@@ -161,21 +211,6 @@ void UART0_IRQHandler(void) {
 		case 5:
 			stav_citania = 0;
 			process_message = 1;
-			break;
-		case 6:
-			message[MESSAGE_SENDER_ADDRESS];
-			stav_citania = 7;
-			break;
-		case 7:
-			message[MESSAGE_RECEIVER_ADDRESS];
-			stav_citania = 8;
-			break;
-		case 8:
-			process_message = 2;
-			stav_citania = 9;
-			break;
-		case 9:
-			stav_citania = 0;
 			break;
 		}
 	}
@@ -212,44 +247,155 @@ int main(void) {
 	engine(ENGINE_DOWN);
 
 	uint8_t direction = ENGINE_DOWN;
+	uint8_t destination = DESTINATION_FLOOR_P;
+	uint8_t position = DESTINATION_FLOOR_P;
 	while (1) {
 		if (process_message == 1) {
 			process_message = 0;
 			ack(message[MESSAGE_SENDER_ADDRESS]);
 			switch (message[MESSAGE_SENDER_ADDRESS]) {
 			case BUTTON_P:
-				led1 = !led1;
-				set_led(led1, LED1_ADDRESS);
+//				led1 = !led1;
+//				set_led(led1, LED1_ADDRESS);
+//				char msg[] = "Test\n";
+//				terminal(msg, sizeof(msg));
+//				destination = DESTINATION_FLOOR_P;
+//				engine(ENGINE_DOWN);
+				destination = DESTINATION_FLOOR_P;
+				switch(position) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_DOWN); break;
+				}
 				break;
 			case BUTTON_1:
-				led2 = !led2;
-				set_led(led2, LED2_ADDRESS);
+//				led2 = !led2;
+//				set_led(led2, LED2_ADDRESS);
+//				destination = DESTINATION_FLOOR_1;
+				destination = DESTINATION_FLOOR_1;
+				switch(position) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_DOWN); break;
+				}
 				break;
 			case BUTTON_2:
-				led3 = !led3;
-				set_led(led3, LED3_ADDRESS);
+//				led3 = !led3;
+//				set_led(led3, LED3_ADDRESS);
+//				door(led3);
+//				destination = DESTINATION_FLOOR_2;
+				destination = DESTINATION_FLOOR_2;
+				switch(position) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_DOWN); break;
+				}
 				break;
 			case BUTTON_3:
-				led4 = !led4;
-				set_led(led4, LED4_ADDRESS);
-				engine(ENGINE_DOWN);
+//				led4 = !led4;
+//				set_led(led4, LED4_ADDRESS);
+//				engine(ENGINE_DOWN);
+				destination = DESTINATION_FLOOR_3;
+				switch(position) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_DOWN); break;
+				}
 				break;
 			case BUTTON_4:
-				led5 = !led5;
-				set_led(led5, LED5_ADDRESS);
-				engine(ENGINE_UP);
+//				led5 = !led5;
+//				set_led(led5, LED5_ADDRESS);
+//				destination = DESTINATION_FLOOR_4;
+//				engine(ENGINE_DOWN);
+				destination = DESTINATION_FLOOR_3;
+				switch(position) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_STOP); break;
+				}
+				break;
 				break;
 
 			case HORNY_NARAZNIK:
 				engine(ENGINE_DOWN);
 				break;
-			case 0xe1:
-				engine(ENGINE_UP);
-				engine_speed();
-				break;
-			default:break;
-			}
 
+			case FLOOR_P_SWITCH:
+				position = DESTINATION_FLOOR_P;
+				switch(destination) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_UP); break;
+				}
+				break;
+			case FLOOR_1_SWITCH:
+				position = DESTINATION_FLOOR_1;
+				switch(destination) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_UP); break;
+				}
+				break;
+			case FLOOR_2_SWITCH:
+				position = DESTINATION_FLOOR_2;
+				switch(destination) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_UP); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_UP); break;
+				}
+				break;
+			case FLOOR_3_SWITCH:
+				position = DESTINATION_FLOOR_3;
+				switch(destination) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_STOP); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_UP); break;
+				}
+				break;
+
+			case FLOOR_4_SWITCH:
+				position = DESTINATION_FLOOR_4;
+				switch(destination) {
+				case DESTINATION_FLOOR_P: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_1: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_2: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_3: engine(ENGINE_DOWN); break;
+				case DESTINATION_FLOOR_4: engine(ENGINE_STOP); break;
+				}
+				break;
+
+			case MY_ADDRESS:
+				if (message[MESSAGE_TYPE] == ACK_BYTE) {
+
+				}
+				break;
+
+			case EMERGENCE_BREAK:
+				if (data[0] == DEACTIVATE_BREAK) {
+				} else {
+					engine(ENGINE_STOP);
+				}
+			default:
+				break;
+			}
 
 		}
 
